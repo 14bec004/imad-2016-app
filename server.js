3ec3067bdd4c2154463d1a2dbbfa2875724ecd45
asashukla <14bec004@smvdu.ac.in>
2016-10-06 23:49:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article one',function(req,res){
+    res.send('ee naya article hai ');
+});
+app.get('/article two',function(req,res){
+    res.send('ee naya article hai no 2');
+});
+app.get('/article three',function(req,res){
+    res.send('ee naya article hai no 3 ');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
