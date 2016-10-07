708399e89d91a40d93d6ee4d942f2c09d0b82d3a
asashukla <14bec004@smvdu.ac.in>
2016-10-07 09:38:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element= document.getElementbyId('main-text');
 element.innerHTML='New value';
-var img= document.getElementbyId('img');
+var img= document.getElementbyId('madi');
 img.onclick = function(){
-    img.style.marginLeft='100px';
+    madi.style.marginLeft='100px';
 };
