bf49ad438dbfafc7f260226881350b126b59d271
asashukla <14bec004@smvdu.ac.in>
2016-10-08 00:53:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 var names=[];
-app.get('/submit_name/:name',function(req,res){
+app.get('/submit-name/:name',function(req,res){
     var name=req.params.name;
     names.push(name);
 res.send(JSON.stringify(names));
